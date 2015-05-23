5e2bf9f984b1b08156658da563ff796de7c8f685
Matthew Beeching <jobe@mdbnet.co.uk>
2015-05-23 19:34:40 +0100

    A few changes to main() regarding engine_init()

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char* argv[])
 {
-    int fork = 1;
+    int fork = 0; /* We'll default this back to 1 later in development. */
 
     log_stdout("core/main", 1, "Starting X4 %s+[%s]", version, cvs_version);
     log_stdout("core/main", 1, "forking");
@@ -19,6 +19,12 @@ int main(int argc, char* argv[])
     engine_init(MAXCONNECTIONS);
 
     init(&fork);
+
+    if (engine_init(MAXCONNECTIONS) != 0)
+    {
+        log_stderr("core/main", 1, "unable to initialize IO engine");
+        return -1;
+    }
 
     while(1) {
         struct TimerInfo *ti = timer_next();
